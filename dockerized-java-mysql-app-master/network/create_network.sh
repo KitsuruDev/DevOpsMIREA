cb85b14af78ad2e8337d09a0ed6bc9501a2e0f80
KitsuruDev <kitsubasedev@gmail.com>
2024-12-11 22:22:49 +0300

    Delete dockerized-java-mysql-app-master/network/create_network.sh

--- dockerized-java-mysql-app-master/network/create_network.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# create an network in order to enable communication between containers throu out container name
-docker network create --driver bridge isolated 
-docker network connect isolated mysqlapp
-docker network connect isolated javaapp
